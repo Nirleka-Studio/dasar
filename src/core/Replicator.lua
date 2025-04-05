6b8eb9d0af7f824319d237ab78aaa3cdc5155903
NirlekaPlay <ainnirleka@gmail.com>
2025-04-05 13:57:36 +0200

    feat: add Replicator

---
+++ src/core/Replicator.lua
@@ -0,0 +1,31 @@
+local GuiService = game:GetService("GuiService")
+local Players = game:GetService("Players")
+
+--[=[
+	@class Replicator
+
+	Replicates several instances not replicated automatically
+	due to several settings.
+
+	For example, if Players.CharacterAutoLoads is set to false,
+	GUIs from StarterGui are not replicated for some reason.
+
+	```lua
+
+	```
+]=]
+local Replicator = {}
+
+function Replicator._ready()
+	if not Players.CharacterAutoLoads then
+		Replicator.ReplicateAllFrom(GuiService, Players.LocalPlayer.PlayerGui)
+	end
+end
+
+function Replicator.ReplicateAllFrom(from: Instance, to: Instance)
+	for _, inst in ipairs(from:GetChildren()) do
+		inst:Clone().parent = to
+	end
+end
+
+return Replicator
