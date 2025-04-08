64fd119bb03a248bacd645de1e63c21dc425d6a2
NirlekaPlay <ainnirleka@gmail.com>
2025-04-08 17:18:23 +0200

    refactor(array): supports negative indexing and non-recursive deep copy
    UNTESTED.

--- src/core/variant/Array.lua
+++ src/core/variant/Array.lua
@@ -9,12 +9,17 @@ local hash_fmix32 = MurmurHash3.fmix32
 
 local MAX_RECURSION = 15
 
-local table = table
 local type = type
+local next = next
 local getmetatable = getmetatable
-local string = string
+local setmetatable = setmetatable
+local table = table
 
 local function neg_index(index, arr)
+	if not type(index) == "number" then
+		return index
+	end
+
 	local len = #arr
 
 	if index < 0 then
@@ -51,10 +56,19 @@ function Array.new()
 	}, Array)
 end
 
-function Array.fromTable(from: {})
+function Array.fromTable(from: { [number]: any })
+	local newArray = Array.new()
+
+	for i, v in ipairs(from) do
+		newArray._data[i] = v
+	end
+
+	return newArray
 end
 
 function Array:__index(index)
+	index = neg_index(index, self._data)
+
 	if Array[index] then
 		return Array[index]
 	else
@@ -66,6 +80,8 @@ function Array:__newindex(index, newValue)
 	if self._readonly then
 		error("Cannot modify a readonly Array!", 4)
 	end
+
+	index = neg_index(index, self._data)
 
 	if type(index) ~= "number" then
 		error(string.format("Cannot index Array with type %s!", typeof(index)), 4)
@@ -92,13 +108,13 @@ end
 
 function Array:_recursive_hash(recursion_count)
 	if recursion_count > MAX_RECURSION then
-		ERR_THROW("Max recursion reached!")
+		error("Max recursion reached!", 4)
 	end
 
 	local h = hash_murmur3_one_32(1)
 	recursion_count = recursion_count + 1
 
-	for key, value in pairs(self._data) do
+	for key, value in ipairs(self._data) do
 		local keyHash = hash_murmur3_one_32(tostring(key):len(), h)
 		local valueHash = hash_murmur3_one_32(tostring(value):len(), h)
 
@@ -127,45 +143,51 @@ function Array:Back()
 	return self._data[#self._data]
 end
 
-function Array:Duplicate(deep: boolean, copies)
-	local dict_copy = Array.new()
-	copies = copies or {}
+function Array:Duplicate(deep: boolean)
+	local new_arr = Array.new()
 
-	if copies[self._data] then
-		return copies[self._data]
-	end
+	if not deep then
 
-	copies[self._data] = dict_copy._data
-
-	local function deepcopy(value)
-		if not deep or type(value) ~= "table" then
-			return value
+		for i, v in ipairs(self._data) do
+			new_arr._data[i] = v
 		end
 
-		if copies[value] then
-			return copies[value]
-		end
-
-		local copy = {}
-		copies[value] = copy
-
-		for k, v in pairs(value) do
-			copy[k] = deepcopy(v)
-		end
-
-		return copy
+		return new_arr
 	end
 
-	for key, value in pairs(self._data) do
-		dict_copy._data[key] = deepcopy(value) -- Copy all values into new dictionary
-	end
+	local stack = {}
+	local copy = {}
+	new_arr[self._data] = copy
+	table.insert(stack, {src = self._data, dst = copy})
 
-	return dict_copy
+	while #stack > 0 do
+		local current = table.remove(stack)
+		local src = current.src
+		local dst = current.dst
+
+		for k, v in pairs(src) do
+			local copy_k = (type(k) == "table") and (new_arr[k] or {}) or k
+			if type(k) == "table" and not new_arr[k] then
+				new_arr[k] = copy_k
+				table.insert(stack, {src = k, dst = copy_k})
+			end
+
+			if type(v) == "table" then
+				if not new_arr[v] then
+					new_arr[v] = {}
+					table.insert(stack, {src = v, dst = new_arr[v]})
+				end
+				dst[copy_k] = new_arr[v]
+			else
+				dst[copy_k] = v
+			end
+		end
+	end
 end
 
 function Array:Erase(value: any)
 	self:_parse_changes()
-	local _, index = self:Has(value)
+	local index = self:Find(value)
 	if not index then
 		return
 	end
@@ -177,7 +199,7 @@ function Array:Front()
 	return self._data[1]
 end
 
-function Array:Find(value, from)
+function Array:Find(value: any, from: number)
 	from = from or 1
 
 	if self:IsEmpty() or from > self:Size() then
@@ -194,6 +216,7 @@ function Array:Find(value, from)
 end
 
 function Array:Get(index: number)
+	index = neg_index(index, self._data)
 	return self._data[index]
 end
 
@@ -219,6 +242,7 @@ end
 
 function Array:Insert(index: number, value: any)
 	self:_parse_changes()
+	index = neg_index(index, self._data)
 
 	if index then
 		return table.insert(self._data, index, value)
@@ -237,6 +261,7 @@ end
 
 function Array:Set(index: number, value: any)
 	self:_parse_changes()
+	index = neg_index(index, self._data)
 	if not self._data[index] then
 		return
 	end
@@ -275,6 +300,7 @@ end
 
 function Array:Remove(index: number)
 	self:_parse_changes()
+	index = neg_index(index, self._data)
 
 	table.remove(self._data, index)
 end
