61b3ae96079eb61c54e3b56dc4e982a78dc5f4d4
NirlekaPlay <ainnirleka@gmail.com>
2025-04-10 18:41:02 +0200

    refactor(dictionary): refactored to match consistency with Array and increase performance

--- src/core/variant/Dictionary.lua
+++ src/core/variant/Dictionary.lua
@@ -2,45 +2,45 @@
 -- NirlekaDev
 -- March 22, 2025
 
---[=[
-	@class Dictionary
-
-	Holds key-value pairs.
-	Based on Godot's Dictionary class.
-]=]
-
 local require = require(game:GetService("ReplicatedStorage").Modules.Dasar).Require
-local ErrorMacros = require("error_macros")
-local MurmurHash3 = require("hash_murmur3")
+local MurmurHash3 = require("../hash/hash_murmur3")
 
 local hash_murmur3_one_32 = MurmurHash3.one_32
 local hash_fmix32 = MurmurHash3.fmix32
 
-local ERR_FAIL_COND_MSG = ErrorMacros.ERR_FAIL_COND_MSG
-local ERR_THROW = ErrorMacros.ERR_THROW
-local ERR_TYPE = ErrorMacros.ERR_TYPE
-
 local MAX_RECURSION = 15
-local HASH_MURMUR3_SEED = 0
 
-local bit32 = bit32
 local type = type
+local next = next
 local getmetatable = getmetatable
+local setmetatable = setmetatable
+local table = table
 
+--[=[
+	@class Dictionary
+
+	Wraps around the table to provide a more convenient API for dictionary-like data structures.
+]=]
 local Dictionary = {}
 Dictionary.__index = Dictionary
 
 setmetatable(Dictionary, {
-	__call = function()
-		return Dictionary.new()
+	__call = function(_, value)
+		return Dictionary._new(value)
 	end
 })
 
 function Dictionary.new()
 	return setmetatable({
 		_data = {},
-		_readonly = false
+		_readonly = false,
+		_hash_cache = 0,
+		_hash_need_update = true
 	}, Dictionary)
+end
+
+function Dictionary._new(value)
+	return Dictionary.toDictionary(value)
 end
 
 function Dictionary.fromTable(from: {})
@@ -61,12 +61,14 @@ function Dictionary:__index(index)
 	end
 end
 
-function Dictionary:__newindex(index, newValue)
-	if self._readonly then
-		ERR_THROW("Cannot modify a readonly Dictionary!")
+function Dictionary:__newindex(index, new_value)
+	if Dictionary[index] then
+		rawset(self, index, new_value)
+
+		return
 	end
 
-	self._data[index] = newValue
+	self:Set(index, new_value)
 end
 
 function Dictionary:__iter()
@@ -77,27 +79,31 @@ function Dictionary:__len()
 	return self:Size()
 end
 
-function Dictionary:__pairs()
-	return next, self._data, nil
+function Dictionary.isDictionary(value)
+	return getmetatable(value) == Dictionary
 end
 
-function Dictionary:__ipairs()
-	return ipairs(self._data)
+function Dictionary.toDictionary(dict)
+	if dict == nil then
+		dict = Dictionary.new()
+	end
+
+	if Dictionary.isDictionary(dict) then
+		return dict
+	end
+
+	return Dictionary.fromTable(dict)
 end
 
-function Dictionary:isDictionary(value)
-	return type(value) == "table" and getmetatable(value) == Dictionary
-end
-
-function Dictionary:recursive_hash(recursion_count)
+function Dictionary:_recursive_hash(recursion_count)
 	if recursion_count > MAX_RECURSION then
-		ERR_THROW("Max recursion reached!")
+		error("Max recursion reached!", 4)
 	end
 
 	local h = hash_murmur3_one_32(1)
 	recursion_count = recursion_count + 1
 
-	for key, value in ipairs(self._data) do
+	for key, value in pairs(self._data) do
 		local keyHash = hash_murmur3_one_32(tostring(key):len(), h)
 		local valueHash = hash_murmur3_one_32(tostring(value):len(), h)
 
@@ -109,47 +115,55 @@ function Dictionary:recursive_hash(recursion_count)
 end
 
 function Dictionary:Clear()
-	table.clear(self._data)
+	for i, _ in pairs(self._data) do
+		self:Set(i, nil)
+	end
 end
 
-function Dictionary:Duplicate(deep: boolean, copies)
-	local dict_copy = Dictionary.new()
-	copies = copies or {}
+function Dictionary:Duplicate(deep: boolean)
+	local new_dict = Dictionary.new()
 
-	if copies[self._data] then
-		return copies[self._data]
-	end
+	if not deep then
 
-	copies[self._data] = dict_copy._data
-
-	local function deepcopy(value)
-		if not deep or type(value) ~= "table" then
-			return value
+		for i, v in ipairs(self._data) do
+			new_dict._data[i] = v
 		end
 
-		if copies[value] then
-			return copies[value]
-		end
-
-		local copy = {}
-		copies[value] = copy
-
-		for k, v in pairs(value) do
-			copy[k] = deepcopy(v)
-		end
-
-		return copy
+		return new_dict
 	end
 
-	for key, value in pairs(self._data) do
-		dict_copy._data[key] = deepcopy(value) -- Copy all values into new dictionary
-	end
+	local stack = {}
+	local copy = {}
+	new_dict[self._data] = copy
+	table.insert(stack, {src = self._data, dst = copy})
 
-	return dict_copy
+	while #stack > 0 do
+		local current = table.remove(stack)
+		local src = current.src
+		local dst = current.dst
+
+		for k, v in pairs(src) do
+			local copy_k = (type(k) == "table") and (new_dict[k] or {}) or k
+			if type(k) == "table" and not new_dict[k] then
+				new_dict[k] = copy_k
+				table.insert(stack, {src = k, dst = copy_k})
+			end
+
+			if type(v) == "table" then
+				if not new_dict[v] then
+					new_dict[v] = {}
+					table.insert(stack, {src = v, dst = new_dict[v]})
+				end
+				dst[copy_k] = new_dict[v]
+			else
+				dst[copy_k] = v
+			end
+		end
+	end
 end
 
 function Dictionary:Erase(key: any)
-	self._data[key] = nil
+	self:Set(key, nil)
 end
 
 function Dictionary:FindKey(value: any)
@@ -163,7 +177,6 @@ function Dictionary:FindKey(value: any)
 end
 
 function Dictionary:Get(key: any, default: any)
-	default = default or nil
 	return self._data[key] or default
 end
 
@@ -181,7 +194,7 @@ function Dictionary:Has(key: any)
 	return self._data[key] ~= nil
 end
 
-function Dictionary:HasAll(keys: {})
+function Dictionary:HasAll(keys: { any })
 	for key, _ in ipairs(keys) do
 		if self._data[key] == nil then
 			return false
@@ -191,19 +204,32 @@ function Dictionary:HasAll(keys: {})
 end
 
 function Dictionary:Hash()
-	return self:recursive_hash(0)
+	if self._hash_need_update then
+		local new_hash = self:_recursive_hash(0)
+		self._hash_cache = new_hash
+		self._hash_need_update = false
+
+		return new_hash
+	else
+		return self._hash_cache
+	end
 end
 
 function Dictionary:IsEmpty()
-	for _ in pairs(self._data) do
-		return false
-	end
-
-	return true
+	return next(self._data) == nil
 end
 
 function Dictionary:IsReadOnly()
-	return self._readonly
+	return self._readonly == true
+end
+
+function Dictionary:Keys()
+	local keys = {}
+	for i, _ in pairs(self._data) do
+		table.insert(keys, i)
+	end
+
+	return keys
 end
 
 function Dictionary:MakeReadOnly()
@@ -211,6 +237,11 @@ function Dictionary:MakeReadOnly()
 end
 
 function Dictionary:Set(key: any, value: any)
+	if self._readonly then
+		error("Cannot modify a readonly Dictionary!", 4)
+	end
+
+	self._hash_need_update = true
 	self._data[key] = value
 end
 
@@ -223,21 +254,17 @@ function Dictionary:Size()
 	return count
 end
 
-function Dictionary:Merge(dictionary, overwrite)
-	ERR_TYPE(dictionary, "dictionary", "table")
-	ERR_TYPE(overwrite, "overwrite", "boolean")
+function Dictionary:Merge(dictionary: { [any] : any}, overwrite: boolean)
+	dictionary = Dictionary.toDictionary(dictionary)
 
 	for key, value in pairs(dictionary) do
 		if self._data[key] == nil or overwrite then
-			self._data[key] = value
+			self:Set(key, value)
 		end
 	end
 end
 
-function Dictionary:Merged(dictionary, overwrite)
-	ERR_TYPE(dictionary, "dictionary", "table")
-	ERR_TYPE(overwrite, "overwrite", "boolean")
-
+function Dictionary:Merged(dictionary: { [any] : any}, overwrite: boolean)
 	local dict_copy = Dictionary.new()
 	for key, value in pairs(self._data) do
 		dict_copy._data[key] = value
@@ -250,6 +277,15 @@ function Dictionary:Merged(dictionary, overwrite)
 	end
 
 	return dict_copy
+end
+
+function Dictionary:Values()
+	local values = {}
+	for _, v in pairs(self._data) do
+		table.insert(values, v)
+	end
+
+	return values
 end
 
 return Dictionary
