aec88fc9a6ab937240cccbb0f74cc1f8d936e71f
NirlekaPlay <ainnirleka@gmail.com>
2025-04-21 10:22:43 +0200

    test(memory): add function demo

--- tests/memory/require_demo.lua
+++ tests/memory/require_demo.lua
@@ -1,44 +1,47 @@
---[=[
-	This will demonstrate and test how require() works in the Roblox enviremount.
-	Thanks to this [DevForum thread](https://devforum.roblox.com/t/module-script-memory/2999826)
-]=]
-
 -- Get and require the ModuleScript
 local ms = script.ModuleScript
 local r = require(ms)
 
 -- Clone the ModuleScript for further testing
--- as the first ModuleScript will be destroyed
 local ms2 = ms:Clone()
 ms2.Parent = ms.Parent
 
 r.yay = "yay"
 
-local r1 = require(ms) -- alwyas refers to the cached 'r'
-
+local r1 = require(ms)
 print(r1.yay) -- "yay", expected
 
--- makes a weak table with weak values
+-- STEP 1: Create a clean standalone copy of the function
+-- This prevents the closure from referencing 'r'
+local cleanFunction = r.while_loop
+r.while_loop = nil -- Break backward reference from 'r' to the function
+
+-- STEP 2: Run the clean function in a spawned task
+local thread = task.spawn(function()
+	cleanFunction()
+end)
+
+-- STEP 3: Prepare for garbage collection
 local gcTest = setmetatable({r; {}}, { __mode = 'v' })
 
-r=nil			-- no further references to the ModuleScript
-ms:Destroy()	-- resulting in garbage collection
+task.cancel(thread)
+thread = nil
+cleanFunction = nil -- Remove last known reference to the function
+r = nil
+ms:Destroy()
 
--- wait until the values gone nil
--- meaning it has been garbage collected
+-- STEP 4: Wait for GC
 while gcTest[2] do
-	print(gcTest[1])
-	print(gcTest[2])
+	print("Still alive:", gcTest[1], gcTest[2])
 	task.wait()
 end
 
 task.wait(1)
 
-print(gcTest[1]) --> nil (garbage collected)
-print(gcTest[2]) --> nil (garbage collected)
+print("After GC:")
+print(gcTest[1]) --> nil
+print(gcTest[2]) --> nil
 
--- if we try to require the second ModuleScript,
--- Roblox will treat this as a seperate module and run it again
--- no further memory of ms
+-- STEP 5: Requiring cloned module again
 local r2 = require(ms2)
-print(r2.yay) -- nil, ofc
+print(r2.yay) -- nil, fresh module
