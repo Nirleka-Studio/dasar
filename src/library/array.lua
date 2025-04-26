5fec28e7b1d1f69735449bf04a23abc9728a08bd
NirlekaPlay <ainnirleka@gmail.com>
2025-04-26 16:12:43 +0200

    feat: completed array.lua

--- src/library/array.lua
+++ src/library/array.lua
@@ -22,6 +22,10 @@ export type Array = {
 	_size_updt: boolean
 }
 
+--[=[
+	@within array
+	Returns a new array.
+]=]
 function array.create(from: Data?)
 	local new_array: Array = {
 		_data = from or {},
@@ -31,6 +35,147 @@ function array.create(from: Data?)
 	}
 
 	return new_array
+end
+
+--[=[
+	@within array
+	Removes all element from the array.
+]=]
+function array.clear(arr: Array)
+	-- no... ive broken the code... every checks shall be in .set() goddamit!!!
+	if arr._readonly then
+		error("Cannot modify a readonly Array", 4)
+	end
+
+	-- but oh well. we need you anyway.
+	table.clear(arr._data)
+	arr._size_updt = true
+end
+
+--[=[
+	@within array
+	Removes the first occurence of index assosicated with `value`
+]=]
+function array.erase(arr: Array, value: any)
+	if arr._readonly then
+		error("Cannot modify a readonly Array", 4)
+	end
+
+	local index = array.find(arr, value)
+	if not index then
+		return
+	end
+
+	table.remove(arr, index)
+	arr._size_updt = true
+end
+
+--[=[
+	@within array
+	Returns the index of the first occurence of `value`
+]=]
+function array.find(arr: Array, value: any, from: number?): number?
+	return table.find(arr._data, value, from)
+end
+
+--[=[
+	@within array
+	Returns the value of `index`
+	Shorthand for array._data[index]
+]=]
+function array.get(arr: Array, index: number): any
+	return arr._data[index]
+end
+
+--[=[
+	@within array
+	Returns true if the array contains `value`
+]=]
+function array.has(arr: Array, value: any): boolean
+	return array.find(arr, value) ~= nil
+end
+
+--[=[
+	@within array
+	Returns true if the array is empty, meaning no entries.
+]=]
+function array.is_empty(arr: Array): boolean
+	return array.size(arr) == 0
+end
+
+--[=[
+	@within array
+	Inserts a new index with `value` at the end of the array.
+]=]
+function array.push_back(arr: Array, value: any)
+	if arr._readonly then
+		error("Cannot modify a readonly Array", 4)
+	end
+
+	arr._data[ array.size(arr) + 1 ] = value
+	arr._size_updt = true
+end
+
+--[=[
+	@within array
+	Removes an existing index from the array.
+	Maintains the order of the array.
+]=]
+function array.remove_at(arr: Array, index: number)
+	if arr._readonly then
+		error("Cannot modify a readonly Array", 4)
+	end
+	arr._size_updt = true
+	return table.remove(arr._data, index)
+end
+
+--[=[
+	@within array
+	Sets the value of `index` to `value`
+]=]
+function array.set(arr: Array, index: number, value: any)
+	if arr._readonly then
+		error("Cannot modify a readonly Array", 4)
+	end
+
+	if type(index) ~= "number" then
+		error(string.format("Cannot index Array with type %s", typeof(index)), 4)
+	end
+
+	local size = array.size(arr)
+
+	if index ~= math.floor(index) then
+		error("Array indices must be integers", 4)
+	end
+
+	if index > (size + 1) then
+		error("Index is out of bounds", 4)
+	end
+
+	if value == nil then
+		table.remove(arr._data, index)
+		arr._size_updt = true
+		return
+	end
+
+	arr._data[index] = value
+	arr._size_updt = true
+end
+
+--[=[
+	@within array
+	Returns the size of the array.
+	Sizes of arrays and cached in the `_size` field.
+	When the array is modified, `_size_updt` is set to true.
+	When `_size_updt` is true, the size of the array is recalculated.
+]=]
+function array.size(arr: Array): number
+	if arr._size_updt then
+		arr._size = #arr._data
+		arr._size_updt = false
+	end
+
+	return arr._size
 end
 
 return array
