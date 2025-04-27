b76c36652b1edf26c6f1d926b87b05d0091823bb
NirlekaPlay <ainnirleka@gmail.com>
2025-04-27 21:37:26 +0200

    refactor(dialogue): DialogueManager.lua now uses the new RTween

--- src/modules/dialogue/DialogueManager.lua
+++ src/modules/dialogue/DialogueManager.lua
@@ -4,43 +4,39 @@
 
 local Players = game:GetService("Players")
 
-local Tween = require("../animation/Tween")
+local rtween = require("../../library/animation/rtween")
 local Dialogue = require("./Dialogue")
 
 local ui = Players.LocalPlayer.PlayerGui:WaitForChild("Dialogue").root
 local ui_dialogue_text: TextLabel = ui.dialogue_backdrop.text
 local ui_dialogue_backdrop: Frame = ui.dialogue_backdrop
 
-local tweens_ui_dialogue = Tween.new()
-	:SetTrans(Tween["ENUM_TRANSITION_TYPES"]["TRANS_CUBIC"])
-	:SetEase(Tween["ENUM_EASING_TYPES"]["EASE_IN_OUT"])
+local tweens_ui_dialogue = rtween.create(Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
 
 local function create_tween(current_tween, trans_type, ease_type)
 	if current_tween then
-		trans_type = current_tween:GetTrans()
-		ease_type = current_tween:GetEase()
-		current_tween:Kill()
+		rtween.kill(current_tween)
 		current_tween = nil
 	end
 
-	--current_tween:Destroy()
-
-	current_tween = Tween.new():SetTrans(trans_type):SetEase(ease_type)
+	current_tween = rtween.create(Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
 	return current_tween
 end
 
 local DialogueManager = {}
 
 function DialogueManager.HideDialogue()
-	local tween = create_tween(tweens_ui_dialogue):SetParallel(true)
-	tween:TweenProperty(ui_dialogue_text, "TextTransparency", 1, 1)
-	tween:TweenProperty(ui_dialogue_backdrop, "Transparency", 1, 1)
+	local tween = create_tween(tweens_ui_dialogue)
+	rtween.set_parallel(tween, true)
+	rtween.tween_instance(tween, ui_dialogue_text, {TextTransparency = 1}, 1)
+	rtween.tween_instance(tween, ui_dialogue_backdrop, {Transparency = 1}, 1)
 end
 
 function DialogueManager.ShowDialogue()
-	local tween = create_tween(tweens_ui_dialogue):SetParallel(true)
-	tween:TweenProperty(ui_dialogue_text, "TextTransparency", 0, 1)
-	tween:TweenProperty(ui_dialogue_backdrop, "Transparency", 0, 1)
+	local tween = create_tween(tweens_ui_dialogue)
+	rtween.set_parallel(tween, true)
+	rtween.tween_instance(tween, ui_dialogue_text, {TextTransparency = 0}, 1)
+	rtween.tween_instance(tween, ui_dialogue_backdrop, {Transparency = 0}, 1)
 end
 
 function DialogueManager.StepText(text: string, config: Dialogue.DiaConfig?)
