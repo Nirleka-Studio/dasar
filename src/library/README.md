7e8f4b2f02bedb5f5a7be50ca88f383af557727f
NirlekaPlay <ainnirleka@gmail.com>
2025-05-08 19:14:58 +0200

    refactor(dsl): fix text skips

--- src/library/README.md
+++ src/library/README.md
@@ -58,3 +58,28 @@ array.push_back(new_array)
 print(array.get(new_array, 5)) -- "e"
 print(array.size(new_array)) -- 5
 ```
+
+
+Some honorable dev notes:
+
+From urich.lua:
+```
+	-- this is more complicated than it needs to be.
+
+	-- all i can explain the steps is that
+	-- 1. Look for the first tag. (<...>) 2. if it does, check if its an attributed tag <...=...>
+	-- 3. if its not, check if its an enclosed that (<...>...</...>)
+
+	-- you'll probably wondering what this is for. well, guess what? its not for markup.
+	-- its the backbone of the dialogue system.
+	-- the previous system uses symbols and internal delays for specific characters (|, _) which is
+	-- a bit too hardcoded and barely extendable.
+	-- so the more logical approach is to just parses the string to a set of specific instrunctions.
+	-- this is an example: in the original system, its like this "Plain text. _INSTANT APPEAR_| wow.|"
+	-- now, this acts as the parser instead of the hard coded engine. So that turns to:
+	-- "Plain text.<wait=1> <skip>INSTANT APPEAR</skip><wait=1> wow.<wait=1>"
+
+	-- what about the ustrings? well, those are wrappers around the normal strings except its unicode array.
+	-- does it overcomplicate it? No. Heck, all of these ustring functions are basically the string library
+	-- functions. And if you did replace them, it'll still work just fine. (if the input is ASCII)`
+```
--- src/library/string/urich.lua
+++ src/library/string/urich.lua
@@ -22,27 +22,18 @@ function urich.parse(text: string)
 	local result = array.create()
 	local u_text = ustring.create(text)
 
-	-- this is more complicated than it needs to be.
-
-	-- all i can explain the steps is that
-	-- 1. Look for the first tag. (<...>) 2. if it does, check if its an attributed tag <...=...>
-	-- 3. if its not, check if its an enclosed that (<...>...</...>)
-
-	-- you'll probably wondering what this is for. well, guess what? its not for markup.
-	-- its the backbone of the dialogue system.
-	-- the previous system uses symbols and internal delays for specific characters (|, _) which is
-	-- a bit too hardcoded and barely extendable.
-	-- so the more logical approach is to just parses the string to a set of specific instrunctions.
-	-- this is an example: in the original system, its like this "Plain text. _INSTANT APPEAR_| wow.|"
-	-- now, this acts as the parser instead of the hard coded engine. So that turns to:
-	-- "Plain text.<wait=1> <skip>INSTANT APPEAR</skip><wait=1> wow.<wait=1>"
-
-	-- what about the ustrings? well, those are wrappers around the normal strings except its unicode array.
-	-- does it overcomplicate it? No. Heck, all of these ustring functions are basically the string library
-	-- functions. And if you did replace them, it'll still work just fine. (if the input is ASCII)
 	local i = 1
 	while i <= u_text.length do
 		local s, e, tag = ustring.sfind(u_text, "<([^>]+)>", i)
+
+		if s and s > i then
+			local plain_text = ustring.sub(u_text, i, s - 1)
+			array.push_back(result, { content = plain_text })
+		elseif not s then
+			local plain_text = ustring.sub(u_text, i)
+			array.push_back(result, { content = plain_text })
+			break
+		end
 
 		if (s and e) then
 			local _, _, att_tag, attribute = string.find(tag, "^(%w+)%s*=%s*(%w+)")
@@ -58,7 +49,7 @@ function urich.parse(text: string)
 				array.push_back(result, { content = plain_text })
 			end
 
-			local s_2, e_2 = ustring.sfind(u_text, "<\/([^>]+)>", e + 1)
+			local s_2, e_2, enclosing = ustring.sfind(u_text, "<\/([^>]+)>", e + 1)
 			if (s_2 and e_2) then
 				local plain_text = ustring.sub(u_text, e + 1, s_2 - 1)
 				array.push_back(result, { tag = tag, content = plain_text })
@@ -78,29 +69,47 @@ end
 
 function urich.step(parsed: Array<Segment>, cps: number, stepper: (chars: string) -> ())
 	local char_delay: number = 1 / cps
+	local default_char_delay: number = char_delay
 	local last_time: number = tick()
 
-	for _, segment in array.iter(parsed) do
-		if segment.tag == "skip" then
+	-- bother even trying?
+	local tag_handlers = {
+		["skip"] = function(segment)
 			stepper(segment.content._string)
-			continue
-		elseif segment.tag == "wait" then
-			local wait_time = segment.content._string
-			if wait_time == "" then
-				wait_time = 0
-			end
+		end,
 
-			task.wait(tonumber(wait_time))
+		["wait"] = function(segment)
+			local wait_time = tonumber(segment.content._string) or 0
+			task.wait(wait_time)
+		end,
+
+		["cps"] = function(segment)
+			local content = segment.content._string
+			if content == "n" then
+				char_delay = default_char_delay
+			else
+				local new_cps = tonumber(content)
+				if new_cps then
+					char_delay = 1 / new_cps
+				end
+			end
+		end,
+	}
+
+	for _, segment in array.iter(parsed) do
+		local tag = segment.tag
+
+		if tag and tag_handlers[tag] then
+			tag_handlers[tag](segment)
 			continue
 		end
 
-		for i, char in array.iter(segment.content._array) do
+		for _, char in array.iter(segment.content._array) do
 			while tick() - last_time < char_delay do
-				task.wait() -- delay so the stack wont go sodding
+				task.wait()
 			end
 
 			stepper(char)
-
 			last_time = tick()
 		end
 	end
